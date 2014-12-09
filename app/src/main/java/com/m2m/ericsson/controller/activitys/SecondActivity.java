7065ce71a1d7a0ec9c3adda5e468a450ef932d84
emLeonorMalaga/em <emingenieroricardopostigo@gmail.com/em>
2014-12-09 13:42:23 +0100

    Primera y segunda vista

---
+++ app/src/main/java/com/m2m/ericsson/controller/activitys/SecondActivity.java
@@ -0,0 +1,7 @@
+package com.m2m.ericsson.controller.activitys;
+
+/**
+ * Created by paco on 09/12/2014.
+ */
+public class SecondActivity {
+}
---
+++ app/src/main/java/com/m2m/ericsson/controller/activitys/SettingsActivity.java
@@ -0,0 +1,7 @@
+package com.m2m.ericsson.controller.activitys;
+
+/**
+ * Created by paco on 20/11/2014.
+ */
+public class SettingsActivity {
+}
